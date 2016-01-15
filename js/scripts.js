75fce09b054f67e3fcf97e196ee8a68c19492044
Chris Young <ctopheryoung@gmail.com>
2016-01-15 12:39:05 -0800

    added branching logic, removed div slides

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 $(document).ready(function() {
-    $("button#begin").click(function() {
-      $("div#introduction-slide").hide();
-      $("div#quiz-slide").show();
-    });
+    $("form#vacationPreferences").submit(function(event) {
+      var temperature = parseInt($("input#temperature").val());
+      var wildMild = $("select#wildMild").val();
+      var roughIt = $("select#roughIt").val();
+      var crowd = $("select#crowd").val();
+      var region = $("select#region").val();
 
-    $("button#answer-submit").click(function(){
-      $("div#quiz-slide").hide();
-      $("div#result-slide").show();
-    });
-
-    $("button#restart").click(function(){
-      $("div#result-slide").hide();
-      $("div#quiz-slide").show();
-    });
-
+      //apply branching logic
+      if (temperature <60) {
+        if (wildMild === "wild") {
+          if (roughIt === "rough") {
+            if (crowd === "loner") {
+              if (region === "global") {
+                var destination = "Whistler, B.C."
+                            $("#destinationName").empty().append(destination);
+                            $("#results").show();
+                          } else {
+                            var destination = "Anchorage, AK"
+                            $("#destinationName").empty().append(destination);
+                            $("#results").show();
+                          }
+              }
+            }
+          }
+        }
+      event.preventDefault();
+    })
   });
