8e0a4051fae123313709e95b0da705a7c720171c
Chris Young <ctopheryoung@gmail.com>
2016-01-15 16:27:17 -0800

    finished adding branching logic and assigned names to each destination

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,107 @@
 $(document).ready(function() {
     $("form#vacationPreferences").submit(function(event) {
-      var temperature = parseInt($("input#temperature").val());
+      var name = ($("input#temperature").val());
       var wildMild = $("select#wildMild").val();
       var roughIt = $("select#roughIt").val();
       var crowd = $("select#crowd").val();
       var region = $("select#region").val();
 
       //apply branching logic
-      if (temperature <60) {
         if (wildMild === "wild") {
           if (roughIt === "rough") {
             if (crowd === "loner") {
               if (region === "global") {
-                  var destination = "Cold, wild, rough, lonely, global place"
+                var destination = "Russia and drink lots of Vodka, comrade" // wild, rough, lonely, global place
                     $("#destinationName").empty().append(destination);
                     $("#results").show();
                 } else {
-                  var destination = "Cold, wild, rough, lonely, local place"
+                  var destination = "Montana for beer tasting and gun shooting" // wild, rough, lonely, local place
                     $("#destinationName").empty().append(destination);
                     $("#results").show();
                 }
               } else {
                 if (region === "global") {
-                    var destination = "Cold, wild, rough, social, global place"
+                  var destination = "Bogota, Columbia" // wild, rough, social, global place
                       $("#destinationName").empty().append(destination);
                       $("#results").show();
                   } else {
-                    var destination = "Cold, wild, rough, social, local place"
+                    var destination = "Burning Man" // wild, rough, social, local place
+                      $("#destinationName").empty().append(destination);
+                      $("#results").show();
+                  }
+              }
+            } else {
+              if (crowd === "loner") {
+                if (region === "global") {
+                  var destination = "Iceland and see the northern lights on LSD" // wild, posh, lonely, global place
+                      $("#destinationName").empty().append(destination);
+                      $("#results").show();
+                  } else {
+                    var destination = "the California Coast and work at a cannibus growing facility" // wild, posh, lonely, local place
+                      $("#destinationName").empty().append(destination);
+                      $("#results").show();
+                  }
+              } else {
+                if (region === "global") {
+                  var destination = "Dubai and gamble your heart out" // wild, posh, social, global place
+                      $("#destinationName").empty().append(destination);
+                      $("#results").show();
+                  } else {
+                    var destination = "Las Vegas and party" // wild, posh, social, local place
                       $("#destinationName").empty().append(destination);
                       $("#results").show();
                   }
               }
             }
+
+        } else {
+          if (roughIt === "rough") {
+            if (crowd === "loner") {
+              if (region === "global") {
+                var destination = "The Great Sahara Desert and go camel riding" // mild, rough, lonely, global place
+                    $("#destinationName").empty().append(destination);
+                    $("#results").show();
+              } else {
+                var destination = "Eastern Oregon and sit in some remote hot springs" // mild, rough, lonely, local place
+                    $("#destinationName").empty().append(destination);
+                    $("#results").show();
+              }
+            } else {
+              if (region === "global") {
+                var destination = "Costa Rica" // mild, rough, social, global place
+                    $("#destinationName").empty().append(destination);
+                    $("#results").show();
+              } else {
+                var destination = "Eugene, Oregon" // mild, rough, social, local place
+                    $("#destinationName").empty().append(destination);
+                    $("#results").show();
+              }
+            }
+          } else {
+            if (crowd === "loner") {
+              if (region === "global") {
+                var destination = "Fiji" // mild, posh, lonely, global place
+                    $("#destinationName").empty().append(destination);
+                    $("#results").show();
+              } else {
+                var destination = "Southern California and rent a beach house" // mild, posh, lonely, local place
+                    $("#destinationName").empty().append(destination);
+                    $("#results").show();
+              }
+            } else {
+              if (region === "global") {
+                var destination = "Paris, France" // mild, posh, social, global place
+                    $("#destinationName").empty().append(destination);
+                    $("#results").show();
+              } else {
+                var destination = "Kentucky and get bougie at the Kentucky Derby" // mild, posh, social, local places
+                    $("#destinationName").empty().append(destination);
+                    $("#results").show();
+              }
+            }
           }
         }
+
       event.preventDefault();
     })
   });
